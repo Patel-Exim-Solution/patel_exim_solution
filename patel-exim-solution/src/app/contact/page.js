67a95af1bd22b11eadb3a214beb02a3585d5e71c
Mihir Prajapati <mihirprajpati135@gmail.com>
2025-11-18 17:37:56 +0530

    New changes

--- patel-exim-solution/src/app/contact/page.js
+++ patel-exim-solution/src/app/contact/page.js
@@ -1,14 +1,14 @@
 "use client";
 import ContactUsForm from "@/components/ContactUsForm";
 import HeaderPageTitle from "@/components/HeaderPageTitle";
-import MyMap from "@/components/MyMap";
+// import MyMap from "@/components/MyMap";
 
 export default function Contact(){
     return (
         <div className="contact-page-container">
             <HeaderPageTitle title={'Contact Us'} parentPage={'Home'} />
             <ContactUsForm />
-            <MyMap />
+            {/* <MyMap /> */}
         </div>
     )
 }
